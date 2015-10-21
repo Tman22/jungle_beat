c25a315d3ecf942c4a0514747030174dc6f1f298
Taylor Moore <Tmoore2272@gmail.com>
2015-10-21 17:16:42 -0600

    End of day 2

--- jungle_beat.rb
+++ jungle_beat.rb
@@ -6,7 +6,6 @@ class Junglebeat
 
 
   def initialize(beats)
-    @count = 0
     make_link_list(beats)
   end                      # => :initialize
 
@@ -18,11 +17,9 @@ class Junglebeat
           node = Node.new(word)
           current.next_node = node
           current = node
-          @count += 1
         else
           @head = Node.new(word)
           current = @head
-          @count +=1
         end
       end
     end
@@ -53,31 +50,117 @@ class Junglebeat
     array.join(" ")
   end                               # => :all
 
-  def append(value)
-    new_node = Node.new(value)
-    find_tail.next_node = new_node
-  end                               # => :append
-
-
-  def prepend(value)
-    
-    current = nil
-    value.split.each do |word|
-      current = Node.new(word)
-      current.next_node = current
+  def count
+    current = @head
+    count = 1
+    until current.next_node == nil
+      count += 1
+      current = current.next_node
     end
-    # new_node = Node.new(value)
-    current.next_node = @head
-    @head = current
+    count
+  end                               # => :count
+
+  def append(value = nil)
+    if value
+      new_head = nil
+      current = nil
+      counter = 1
+      value.split.each do |word|
+        if current
+          node = Node.new(word)
+          current.next_node = node
+          current = node
+          counter +=1
+        else
+          new_head = Node.new(word)
+          current = new_head
+        end
+      end
+      find_tail.next_node = current
+      counter
+    end
+
+  end  # => :append
 
 
-  end  # => :prepend
+  def prepend(value =nil)
+    if value
+      new_head = nil
+      current = nil
+      counter = 1
+      value.split.each do |word|
+        if new_head
+          node = Node.new(word)
+          current.next_node = node
+          current = node
+          counter +=1
+        else
+          new_head = Node.new(word)
+          current = new_head
+        end
+        current.next_node = @head
+      end
+      @head = new_head
+      counter
+    end
+  end                                # => :prepend
 
-#   def count
-#
-#
-#   end
-#
-#
-#
-end  # => :prepend
+  def include?(data)
+    current = @head
+    until current.next_node == nil
+      current = current.next_node
+      if current.node == data
+         return true
+      end
+    end
+    false
+  end                               # => :include?
+
+  def pop(number = 1)
+    current = @head
+    array = []
+    number.times do
+      while current.next_node.next_node != nil
+            current = current.next_node
+          end
+              array << current.next_node.node
+              current.next_node = nil
+              current = @head
+        end
+        array.reverse.join(" ")
+    end                                         # => :pop
+
+
+  def insert(position, value)
+
+  end  # => :insert
+
+  def find(position, amount = 1)
+    current = @head
+    array = []
+    binding.pry
+    if count < (position + amount)
+      "NO"
+    else
+    counter = 1
+    amount.times do
+      until counter == position
+          current = current.next_node
+          counter +=1
+        end
+        array << current.next_node.node
+        position += 1
+        counter = 1
+        current = @head
+      end
+    array.join(" ")
+    end
+  end                # => :find
+
+  def play
+    `say -r 500 -v Boing "#{self.all}"`
+    count
+  end       # => :play
+
+
+end  # => :play
--- jungle_beat_test.rb
+++ jungle_beat_test.rb
@@ -1,88 +1,119 @@
 require 'minitest'              # => true
 require 'minitest/autorun'      # => true
+require 'minitest/pride'        # => true
 require_relative 'jungle_beat'  # => true
 
 class Jungle_Beat_Test < Minitest::Test  # => Minitest::Test
 
   def test_it_has_a_head
-    jb = Junglebeat.new("dee")  # => #<Junglebeat:0x007fae09193cc8 @count=1, @head=#<Node:0x007fae09193bb0 @node="dee", @next_node=nil>>
+    jb = Junglebeat.new("dee")  # => #<Junglebeat:0x007fa4528e2738 @head=#<Node:0x007fa4528e26c0 @node="dee", @next_node=nil>>
 
     assert_equal "dee", jb.head_value  # => true
   end                                  # => :test_it_has_a_head
 
   def test_head_works_with_multiple_object
-    jb = Junglebeat.new("dee tee bee")      # => #<Junglebeat:0x007fae09192af8 @count=3, @head=#<Node:0x007fae09192940 @node="dee", @next_node=#<Node:0x007fae091928f0 @node="tee", @next_node=#<Node:0x007fae091928a0 @node="bee", @next_node=nil>>>>
+    jb = Junglebeat.new("dee tee bee")      # => #<Junglebeat:0x007fa4520373b8 @head=#<Node:0x007fa452037a70 @node="dee", @next_node=#<Node:0x007fa452037ca0 @node="tee", @next_node=#<Node:0x007fa452037de0 @node="bee", @next_node=nil>>>>
 
     assert_equal "dee", jb.head_value  # => true
   end                                  # => :test_head_works_with_multiple_object
 
   def test_it_has_a_tail
-    jb = Junglebeat.new("dee deep beep tee wee")  # => #<Junglebeat:0x007fae091822e8 @count=5, @head=#<Node:0x007fae09182130 @node="dee", @next_node=#<Node:0x007fae091820b8 @node="deep", @next_node=#<Node:0x007fae09182090 @node="beep", @next_node=#<Node:0x007fae09182068 @node="tee", @next_node=#<Node:0x007fae09182018 @node="wee", @next_node=nil>>>>>>
+    jb = Junglebeat.new("dee deep beep tee wee")  # => #<Junglebeat:0x007fa4528e0e60 @head=#<Node:0x007fa4528e0c80 @node="dee", @next_node=#<Node:0x007fa4528e0c58 @node="deep", @next_node=#<Node:0x007fa4528e0be0 @node="beep", @next_node=#<Node:0x007fa4528e0bb8 @node="tee", @next_node=#<Node:0x007fa4528e0b68 @node="wee", @next_node=nil>>>>>>
 
-    assert_equal "wee", jb.find_tail
-  end                                 # => :test_it_has_a_tail
+    assert_equal "wee", jb.find_tail.node  # => true
+  end                                      # => :test_it_has_a_tail
 
 
   def test_all_for_empty_data
-    jb = Junglebeat.new
+    skip
+    jb = Junglebeat.new()
 
     assert_equal nil, jb.all
   end                         # => :test_all_for_empty_data
 
 
   def test_all_for_multiple_data
-    jb = Junglebeat.new("dee deep")  # => #<Junglebeat:0x007fae09190190 @count=2, @head=#<Node:0x007fae09190528 @node="dee", @next_node=#<Node:0x007fae091902d0 @node="deep", @next_node=nil>>>
-    jb.all                           # => "dee deep"
-    assert_equal "dee tee", jb.all
+    jb = Junglebeat.new("dee deep")  # => #<Junglebeat:0x007fa4528e1838 @head=#<Node:0x007fa4528e1798 @node="dee", @next_node=#<Node:0x007fa4528e1770 @node="deep", @next_node=nil>>>
+
+    assert_equal "dee deep", jb.all  # => true
   end                                # => :test_all_for_multiple_data
 
 
   def test_append_data
-    jb = Junglebeat.new("dee weee tee")  # => #<Junglebeat:0x007fae09183bc0 @count=3, @head=#<Node:0x007fae09183940 @node="dee", @next_node=#<Node:0x007fae09183918 @node="weee", @next_node=#<Node:0x007fae09183878 @node="tee", @next_node=nil>>>>
-    jb.append("deep")                    # => #<Node:0x007fae09183058 @node="deep", @next_node=nil>
-
-    assert_equal "deep", jb.find_tail.node    # => true
-    assert_equal "dee weee tee deep", jb.all  # => true
+    jb = Junglebeat.new("dee weee tee")       # => #<Junglebeat:0x007fa45202ca08 @head=#<Node:0x007fa45202c8f0 @node="dee", @next_node=#<Node:0x007fa45202c8c8 @node="weee", @next_node=#<Node:0x007fa45202c878 @node="tee", @next_node=nil>>>>
+    jb.all                                    # => "dee weee tee"
+    assert_equal 2, jb.append("deep teep")    # => true
+    jb.all                                    # => "dee weee tee teep"
+    assert_equal "teep", jb.find_tail.node    # => true
+    assert_equal "dee weee tee deep teep", jb.all  # => true
   end                                         # => :test_append_data
+
+  def test_append_data_empty
+    jb = Junglebeat.new("dee dee beat")  # => #<Junglebeat:0x007fa4528e8728 @head=#<Node:0x007fa4528e8638 @node="dee", @next_node=#<Node:0x007fa4528e85c0 @node="dee", @next_node=#<Node:0x007fa4528e8598 @node="beat", @next_node=nil>>>>
+
+    assert_equal nil, jb.append  # => true
+  end                            # => :test_append_data_empty
 
 
   def test_prepend_data
-    jb = Junglebeat.new("dee tee bee")  # => #<Junglebeat:0x007fae091704a8 @count=3, @head=#<Node:0x007fae09170368 @node="dee", @next_node=#<Node:0x007fae09170340 @node="tee", @next_node=#<Node:0x007fae09170318 @node="bee", @next_node=nil>>>>
-    jb.prepend("beet tee")
-    jb.all
-    assert_equal "beet", jb.head_value
-    assert_equal "beet tee dee tee bee", jb.all
-  end                                   # => :test_prepend_data
+    jb = Junglebeat.new("dee tee bee")           # => #<Junglebeat:0x007fa452024a60 @head=#<Node:0x007fa4520248d0 @node="dee", @next_node=#<Node:0x007fa4520248a8 @node="tee", @next_node=#<Node:0x007fa452024858 @node="bee", @next_node=nil>>>>
+    assert_equal 2, jb.prepend("beet tee")       # => true
+    jb.all                                       # => "beet tee dee tee bee"
+    assert_equal "beet", jb.head_value           # => true
+    assert_equal "beet tee dee tee bee", jb.all  # => true
+  end                                            # => :test_prepend_data
+
+  def test_prepend_data_empty
+    jb = Junglebeat.new("de dea tea")  # => #<Junglebeat:0x007fa452025d48 @head=#<Node:0x007fa452025b40 @node="de", @next_node=#<Node:0x007fa452025aa0 @node="dea", @next_node=#<Node:0x007fa452025a78 @node="tea", @next_node=nil>>>>
+    assert_equal nil, jb.prepend       # => true
+  end                                  # => :test_prepend_data_empty
 
 
   def test_make_link_list
-    jb = Junglebeat.new("dee tee deep")  # => #<Junglebeat:0x007fae09178130 @count=3, @head=#<Node:0x007fae09178888 @node="dee", @next_node=#<Node:0x007fae09178b30 @node="tee", @next_node=#<Node:0x007fae09179698 @node="deep", @next_node=nil>>>>
-    assert_equal "deep", jb.find_tail
-    assert_equal "dee", jb.head_value
-  end                                    # => :test_make_link_list
+    jb = Junglebeat.new("dee tee deep")  # => #<Junglebeat:0x007fa4528dbc30 @head=#<Node:0x007fa4528dbac8 @node="dee", @next_node=#<Node:0x007fa4528dbaa0 @node="tee", @next_node=#<Node:0x007fa4528dba78 @node="deep", @next_node=nil>>>>
+
+    assert_equal "deep", jb.find_tail.node  # => true
+    assert_equal "dee", jb.head_value       # => true
+  end                                       # => :test_make_link_list
 
   def test_does_it_count_nodes
-    jb = Junglebeat.new("dee tee deep")  # => #<Junglebeat:0x007fae0918af60 @count=3, @head=#<Node:0x007fae0918ad08 @node="dee", @next_node=#<Node:0x007fae0918ace0 @node="tee", @next_node=#<Node:0x007fae0918ac40 @node="deep", @next_node=nil>>>>
+    jb = Junglebeat.new("dee tee deep")  # => #<Junglebeat:0x007fa4528e3958 @head=#<Node:0x007fa4528e3750 @node="dee", @next_node=#<Node:0x007fa4528e3610 @node="tee", @next_node=#<Node:0x007fa4528e35e8 @node="deep", @next_node=nil>>>>
 
     assert_equal 3, jb.count  # => true
   end                         # => :test_does_it_count_nodes
 
   def test_for_insert
     skip
-    #use insert_variable = count iterate through that.
-  end                  # => :test_for_insert
+    jb = Junglebeat.new("wee tee bee mee")
+    jb.insert(3, "see beat")
+    jb.all
+    assert_equal ("wee tee see beat bee mee"), jb.all
+  end                                                  # => :test_for_insert
 
   def test_find_returns_object
-    skip
-  end                           # => :test_find_returns_object
+    jb = Junglebeat.new("bee tee see mee kee lee dee")  # => #<Junglebeat:0x007fa45226eb68 @head=#<Node:0x007fa45226e938 @node="bee", @next_node=#<Node:0x007fa45226e8e8 @node="tee", @next_node=#<Node:0x007fa45226e870 @node="see", @next_node=#<Node:0x007fa45226e848 @node="mee", @next_node=#<Node:0x007fa45226e7d0 @node="kee", @next_node=#<Node:0x007fa45226e7a8 @node="lee", @next_node=#<Node:0x007fa45226e780 @node="dee", @next_node=nil>>>>>>>>
+
+    assert_equal "mee kee lee dee", jb.find(3, 4)  # => true
+    assert_equal "NO", jb.find(7,9)                # => true
+    assert_equal "mee kee", jb.find(3, 2)          # => true
+  end                                              # => :test_find_returns_object
 
   def test_include_finds_object_returns_true
-    skip
+    jb = Junglebeat.new("dee tee deep wee")   # => #<Junglebeat:0x007fa45202f168 @head=#<Node:0x007fa45202f028 @node="dee", @next_node=#<Node:0x007fa45202efd8 @node="tee", @next_node=#<Node:0x007fa45202ef88 @node="deep", @next_node=#<Node:0x007fa45202ec68 @node="wee", @next_node=nil>>>>>
+    jb.all                                    # => "dee tee deep wee"
+    jb.include?("beat")                       # => false
+    assert jb.include?("tee")                 # => true
+    refute jb.include?("beat")                # => false
   end                                         # => :test_include_finds_object_returns_true
 
   def test_pop_removes_last_object
-    skip
-  end                               # => :test_pop_removes_last_object
+    jb = Junglebeat.new("dee tees deep wee")  # => #<Junglebeat:0x007fa4528e9c18 @head=#<Node:0x007fa4528e9b00 @node="dee", @next_node=#<Node:0x007fa4528e9ad8 @node="tees", @next_node=#<Node:0x007fa4528e9ab0 @node="deep", @next_node=#<Node:0x007fa4528e9a88 @node="wee", @next_node=nil>>>>>
+
+
+    assert_equal "wee", jb.pop           # => true
+    assert_equal "tees deep", jb.pop(2)  # => true
+    assert_equal "dee", jb.all           # => true
+  end                                    # => :test_pop_removes_last_object
 
 
 
@@ -93,54 +124,89 @@ class Jungle_Beat_Test < Minitest::Test  # => Minitest::Test
 
 end  # => :test_pop_removes_last_object
 
-# >> Run options: --seed 43516
+# >> Run options: --seed 48518
 # >>
 # >> # Running:
 # >>
-# >> ..F.SS.FSFSEE
+# >> .....
+# >> \e[1mFrom:\e[0m /Users/taylormoore/turing/1module/jungle_beat/jungle_beat.rb @ line 141 Junglebeat#find:
 # >>
-# >> Finished in 0.016151s, 804.8916 runs/s, 495.3179 assertions/s.
+# >>     \e[1;34m138\e[0m: \e[32mdef\e[0m \e[1;34mfind\e[0m(position, amount = \e[1;34m1\e[0m)
+# >>     \e[1;34m139\e[0m:   current = @head
+# >>     \e[1;34m140\e[0m:   array = []
+# >>  => \e[1;34m141\e[0m:   binding.pry
+# >>     \e[1;34m142\e[0m:   \e[32mif\e[0m count < (position + amount)
+# >>     \e[1;34m143\e[0m:     \e[31m\e[1;31m"\e[0m\e[31mNO\e[1;31m"\e[0m\e[31m\e[0m
+# >>     \e[1;34m144\e[0m:   \e[32melse\e[0m
+# >>     \e[1;34m145\e[0m:   counter = \e[1;34m1\e[0m
+# >>     \e[1;34m146\e[0m:   amount.times \e[32mdo\e[0m
+# >>     \e[1;34m147\e[0m:     \e[32muntil\e[0m counter == position
+# >>     \e[1;34m148\e[0m:         current = current.next_node
+# >>     \e[1;34m149\e[0m:         counter +=\e[1;34m1\e[0m
+# >>     \e[1;34m150\e[0m:       \e[32mend\e[0m
+# >>     \e[1;34m151\e[0m:       array << current.next_node.node
+# >>     \e[1;34m152\e[0m:       position += \e[1;34m1\e[0m
+# >>     \e[1;34m153\e[0m:       counter = \e[1;34m1\e[0m
+# >>     \e[1;34m154\e[0m:       current = @head
+# >>     \e[1;34m155\e[0m:     \e[32mend\e[0m
+# >>     \e[1;34m156\e[0m:   array.join(\e[31m\e[1;31m"\e[0m\e[31m \e[1;31m"\e[0m\e[31m\e[0m)
+# >>     \e[1;34m157\e[0m:   \e[32mend\e[0m
+# >>     \e[1;34m158\e[0m: \e[32mend\e[0m                \e[1;34m# => :find\e[0m
 # >>
-# >>   1) Failure:
-# >> Jungle_Beat_Test#test_all_for_multiple_data [/Users/taylormoore/turing/1module/jungle_beat/jungle_beat_test.rb:36]:
-# >> Expected: "dee tee"
-# >>   Actual: "dee deep"
+# >> \e[0G\e[?1034h[1] pry(#<Junglebeat>)>
+# >> \e[1mFrom:\e[0m /Users/taylormoore/turing/1module/jungle_beat/jungle_beat.rb @ line 141 Junglebeat#find:
 # >>
+# >>     \e[1;34m138\e[0m: \e[32mdef\e[0m \e[1;34mfind\e[0m(position, amount = \e[1;34m1\e[0m)
+# >>     \e[1;34m139\e[0m:   current = @head
+# >>     \e[1;34m140\e[0m:   array = []
+# >>  => \e[1;34m141\e[0m:   binding.pry
+# >>     \e[1;34m142\e[0m:   \e[32mif\e[0m count < (position + amount)
+# >>     \e[1;34m143\e[0m:     \e[31m\e[1;31m"\e[0m\e[31mNO\e[1;31m"\e[0m\e[31m\e[0m
+# >>     \e[1;34m144\e[0m:   \e[32melse\e[0m
+# >>     \e[1;34m145\e[0m:   counter = \e[1;34m1\e[0m
+# >>     \e[1;34m146\e[0m:   amount.times \e[32mdo\e[0m
+# >>     \e[1;34m147\e[0m:     \e[32muntil\e[0m counter == position
+# >>     \e[1;34m148\e[0m:         current = current.next_node
+# >>     \e[1;34m149\e[0m:         counter +=\e[1;34m1\e[0m
+# >>     \e[1;34m150\e[0m:       \e[32mend\e[0m
+# >>     \e[1;34m151\e[0m:       array << current.next_node.node
+# >>     \e[1;34m152\e[0m:       position += \e[1;34m1\e[0m
+# >>     \e[1;34m153\e[0m:       counter = \e[1;34m1\e[0m
+# >>     \e[1;34m154\e[0m:       current = @head
+# >>     \e[1;34m155\e[0m:     \e[32mend\e[0m
+# >>     \e[1;34m156\e[0m:   array.join(\e[31m\e[1;31m"\e[0m\e[31m \e[1;31m"\e[0m\e[31m\e[0m)
+# >>     \e[1;34m157\e[0m:   \e[32mend\e[0m
+# >>     \e[1;34m158\e[0m: \e[32mend\e[0m                \e[1;34m# => :find\e[0m
 # >>
-# >>   2) Failure:
-# >> Jungle_Beat_Test#test_it_has_a_tail [/Users/taylormoore/turing/1module/jungle_beat/jungle_beat_test.rb:22]:
-# >> --- expected
-# >> +++ actual
-# >> @@ -1 +1 @@
-# >> -"wee"
-# >> +#<Node:0xXXXXXX @node="wee", @next_node=nil>
+# >> \e[0G[1] pry(#<Junglebeat>)>
+# >> \e[1mFrom:\e[0m /Users/taylormoore/turing/1module/jungle_beat/jungle_beat.rb @ line 141 Junglebeat#find:
 # >>
+# >>     \e[1;34m138\e[0m: \e[32mdef\e[0m \e[1;34mfind\e[0m(position, amount = \e[1;34m1\e[0m)
+# >>     \e[1;34m139\e[0m:   current = @head
+# >>     \e[1;34m140\e[0m:   array = []
+# >>  => \e[1;34m141\e[0m:   binding.pry
+# >>     \e[1;34m142\e[0m:   \e[32mif\e[0m count < (position + amount)
+# >>     \e[1;34m143\e[0m:     \e[31m\e[1;31m"\e[0m\e[31mNO\e[1;31m"\e[0m\e[31m\e[0m
+# >>     \e[1;34m144\e[0m:   \e[32melse\e[0m
+# >>     \e[1;34m145\e[0m:   counter = \e[1;34m1\e[0m
+# >>     \e[1;34m146\e[0m:   amount.times \e[32mdo\e[0m
+# >>     \e[1;34m147\e[0m:     \e[32muntil\e[0m counter == position
+# >>     \e[1;34m148\e[0m:         current = current.next_node
+# >>     \e[1;34m149\e[0m:         counter +=\e[1;34m1\e[0m
+# >>     \e[1;34m150\e[0m:       \e[32mend\e[0m
+# >>     \e[1;34m151\e[0m:       array << current.next_node.node
+# >>     \e[1;34m152\e[0m:       position += \e[1;34m1\e[0m
+# >>     \e[1;34m153\e[0m:       counter = \e[1;34m1\e[0m
+# >>     \e[1;34m154\e[0m:       current = @head
+# >>     \e[1;34m155\e[0m:     \e[32mend\e[0m
+# >>     \e[1;34m156\e[0m:   array.join(\e[31m\e[1;31m"\e[0m\e[31m \e[1;31m"\e[0m\e[31m\e[0m)
+# >>     \e[1;34m157\e[0m:   \e[32mend\e[0m
+# >>     \e[1;34m158\e[0m: \e[32mend\e[0m                \e[1;34m# => :find\e[0m
 # >>
+# >> \e[0G[1] pry(#<Junglebeat>)> .SS.......
 # >>
-# >>   3) Failure:
-# >> Jungle_Beat_Test#test_make_link_list [/Users/taylormoore/turing/1module/jungle_beat/jungle_beat_test.rb:59]:
-# >> --- expected
-# >> +++ actual
-# >> @@ -1 +1 @@
-# >> -"deep"
-# >> +#<Node:0xXXXXXX @node="deep", @next_node=nil>
+# >> Finished in 0.045126s, 332.4014 runs/s, 509.6821 assertions/s.
 # >>
-# >>
-# >>
-# >>   4) Error:
-# >> Jungle_Beat_Test#test_all_for_empty_data:
-# >> ArgumentError: wrong number of arguments (0 for 1)
-# >>     /Users/taylormoore/turing/1module/jungle_beat/jungle_beat.rb:8:in `initialize'
-# >>     /Users/taylormoore/turing/1module/jungle_beat/jungle_beat_test.rb:27:in `new'
-# >>     /Users/taylormoore/turing/1module/jungle_beat/jungle_beat_test.rb:27:in `test_all_for_empty_data'
-# >>
-# >>
-# >>   5) Error:
-# >> Jungle_Beat_Test#test_prepend_data:
-# >> NameError: undefined local variable or method `current' for #<Junglebeat:0x007fae091704a8>
-# >>     /Users/taylormoore/turing/1module/jungle_beat/jungle_beat.rb:63:in `prepend'
-# >>     /Users/taylormoore/turing/1module/jungle_beat/jungle_beat_test.rb:51:in `test_prepend_data'
-# >>
-# >> 13 runs, 8 assertions, 3 failures, 2 errors, 4 skips
+# >> 15 runs, 23 assertions, 0 failures, 0 errors, 2 skips
 # >>
 # >> You have skipped tests. Run with --verbose for details.
